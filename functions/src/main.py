996875572223f65c3a03f84e64fb93c310f4d643
Douglas Luan <douglasctm@gmail.com>
2020-02-29 12:56:44 -0300

    adding the new methods for handling dialogs with different algorithms - PRED and TRUE Tested with the three urls

--- functions/src/main.py
+++ functions/src/main.py
@@ -24,7 +24,7 @@ db = firestore.client()
 # Initialize intent handler facade 
 agent = IntentHandler(db)
 
-def get_dialogflow_fulfillment(request, agent=agent):
+def get_dialogflow_fulfillment(request, algo_type, agent=agent):
     """Responds to Dialogflow fulfillment webhook request.
     Args:
         request (flask.Request): HTTP request object.
@@ -37,11 +37,19 @@ def get_dialogflow_fulfillment(request, agent=agent):
     request_json = request.get_json()
     query_result = request_json['queryResult']
 
+    # Record interaction
+    if algo_type == "TRUE":
+        db.collection('interações-graph').document().set(request_json)
+    elif algo_type == "PRED":
+        db.collection('interações').document().set(request_json)
+
     intent = query_result['intent']['displayName']
     params = query_result['parameters']
     # context = {}
     # if 'outputContexts' in query_result:
     #     context = query_result['outputContexts']
+    # params['outputContexts'] = context
+    params['algo_type'] = algo_type
 
     res = agent.handle_intent(intent, params)
 
